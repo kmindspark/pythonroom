53178dd78b481cecee76d65d8a619a37252adade
kmindspark <19kaushiks@gmail.com>
2014-05-11 13:33:27 -0700

    initial commit

---
+++ spikyball.py
@@ -0,0 +1,22 @@
+# author: kmindspark
+import turtle
+t=turtle.Turtle()
+
+numbers = range(0, 5)
+for i in numbers:
+	t.forward(100)
+	t.right(144)
+
+
+
+t.right(144)
+t.forward(100)
+t.right(144)
+t.forward(100)
+t.right(144)
+t.forward(100)
+t.right(144)
+t.forward(100)
+t.right(144)
+t.forward(100)
+
