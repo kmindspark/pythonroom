82aa2d57199079d5dbff8d16cba380bc4269edf3
kmindspark <19kaushiks@gmail.com>
2014-05-11 13:35:39 -0700

    pythonroom update

--- spikyball.py
+++ spikyball.py
@@ -2,21 +2,5 @@
 import turtle
 t=turtle.Turtle()
 
-numbers = range(0, 5)
-for i in numbers:
-	t.forward(100)
-	t.right(144)
-
-
-
-t.right(144)
-t.forward(100)
-t.right(144)
-t.forward(100)
-t.right(144)
-t.forward(100)
-t.right(144)
-t.forward(100)
-t.right(144)
-t.forward(100)
-
+numbers = range (1,200)
+for i 
