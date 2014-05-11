0ef3c520f9524d987289ee9e4648153e93144b73
kmindspark <19kaushiks@gmail.com>
2014-05-11 14:58:35 -0700

    pythonroom update

--- primesum.py
+++ primesum.py
@@ -1,1 +1,14 @@
 # author: kmindspark
+n = int (input ("What number would you like to check?"))
+tests = range (2, 503)
+prime = True
+
+if prime = True:
+	for t in tests:
+		if n % t == 0:
+			print "Composite"
+			prime = False
+		else:
+			print "Prime"
+			
+print ("Done")
