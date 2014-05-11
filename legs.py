058b8d48c5da46aa52602677fa41984db4f93d2d
kmindspark <19kaushiks@gmail.com>
2014-05-11 14:48:35 -0700

    draws a sprite with inputted number of legs

--- legs.py
+++ legs.py
@@ -2,3 +2,13 @@
 import turtle
 
 t = turtle.Turtle()
+legs = int (input ("How many legs do you want the sprite to have?"))
+print legs
+
+t.shape ("circle")
+
+for i in range (legs):
+	t.forward (100)
+	t.stamp()
+	t.backward (100)
+	t.right ((360/(legs)))
