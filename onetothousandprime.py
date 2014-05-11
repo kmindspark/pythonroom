33fca982de128373348065b780180d9fed353736
kmindspark <19kaushiks@gmail.com>
2014-05-11 15:17:21 -0700

    listing out prime numbers and adding them

--- onetothousandprime.py
+++ onetothousandprime.py
@@ -1,11 +1,17 @@
 # author: kmindspark
-tests = range (2, 1001)
-prime = True
-
-if prime == True:
-	for t in tests:
-		if n % t == 0:
-			prime = False
-
-if prime == True:
-	print str (n) + " is prime."
+total = 0
+for n in range (2, 1001):
+	
+	tests = range (2, n-1)
+	prime = True
+	
+	if prime == True:
+		for t in tests:
+			if n % t == 0:
+				prime = False
+	
+	if prime == True:
+		total = total + n
+		print str (n) + " is prime."
+		
+print str (total) + " is their sum."
